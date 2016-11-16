20c90a8e0a716d707a6d4f8cae215d4e38b342e4
Hafizur Rahman <hafuzur.rahmanc@gmail.com>
2016-11-16 14:56:13 -0500

    Fixed merged conflict

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-<<<<<<< HEAD
-  # before_action :authenticate_user!
-=======
+  before_action :authenticate_user!
 
 
   def after_sign_in_path_for(resource_or_scope)
@@ -15,6 +13,4 @@ class ApplicationController < ActionController::Base
    root_path
   end
 
-
->>>>>>> 3d759aae8a340a5455db9cad8e9ed93105392fc5
 end
