e5094bf56d498af73919f8dda91e721c7d1085ac
Flatiron School <github email address>
2016-11-17 00:10:34 -0500

    navigation bar links and welcome index page changed

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,12 @@
 class WelcomeController < ApplicationController
 
+  # before_filter :authenticate_user!, except: [:index]
+  
+  skip_before_action :authenticate_user!, only: [:index]
   def index
   end
 
 
 
-  
+
 end
