d8f63d99101f87421894d7765601013a7f29521b
Naveed Shah <naveed@tayybah.com>
2016-11-26 11:40:08 -0500

    deleted .swp files

--- app/assets/javascripts/comments.js
+++ app/assets/javascripts/comments.js
@@ -1,12 +1,11 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
   $("a.load_comments").on('click', function(e) {
   e.preventDefault();
   var id = $(this).data("id");
   $.get("/plans/" + id + ".json", function(data) {
       data.forEach(function(comment) {
-        $("#comment-" + id).html('<p>' + comment + '</p>');
-      }); 
+        $("#comment-" + id).append('<p>' + comment + '</p>');
+      });
     });
   });
 });
-
